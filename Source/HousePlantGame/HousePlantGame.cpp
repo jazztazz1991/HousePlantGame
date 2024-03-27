9f5c5a6ee53c70dd35eeb3c9b959d7015983af25
Cody Chase <cchase1991@gmail.com>
2024-03-27 13:29:49 -0400

    Initial commit

---
+++ Source/HousePlantGame/HousePlantGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HousePlantGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HousePlantGame, "HousePlantGame" );
